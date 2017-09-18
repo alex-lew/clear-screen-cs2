9a3faf345d2ab22d8911a91294168f6cbb7db8b5
Alex Lew <alexl.mail@gmail.com>
2017-09-17 22:09:05 -0400

    Add clear package

---
+++ clear-screen-cs2.go
@@ -0,0 +1,38 @@
+/* Adapted from https://stackoverflow.com/questions/22891644/how-can-i-clear-the-terminal-screen-in-go
+ * for use in CS 2 at Commonwealth School.
+ */
+
+package clear
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["darwin"] = clear["linux"]
+	clear["windows"] = func() {
+		cmd := exec.Command("cls") //Windows example it is untested, but I think its working
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+// WipeScreen clears the Terminal screen and resets cursor to top left.
+func WipeScreen() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
